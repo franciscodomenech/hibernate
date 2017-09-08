ad37d3d285ddd6e08b200aed19ed6e6f1667b625
testcursojava <31422115+testcursojava@users.noreply.github.com>
2017-09-08 08:40:36 +0200

    resuelto problema lazy

--- SpringHibernateMVC/src/main/java/com/cursocuencajava/app/entities/Usuario.java
+++ SpringHibernateMVC/src/main/java/com/cursocuencajava/app/entities/Usuario.java
@@ -1,11 +1,15 @@
 package com.cursocuencajava.app.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -35,12 +39,12 @@ public class Usuario implements Serializable, IJson {
 	@Column(name = "tipo")
 	private int tipo;
 	
-	@OneToMany(cascade=CascadeType.ALL)
+	@OneToMany(fetch = FetchType.EAGER,cascade=CascadeType.ALL)
 	@JoinTable(name="preferencias", 
 	    joinColumns={ @JoinColumn(name="user")},
 	    inverseJoinColumns={ @JoinColumn(name="art")}
 			)
-	private List<Categoria> preferencias;
+	private Collection<Categoria> preferencias = new LinkedHashSet<Categoria>();
 
 	public Long getId() {
 		return id;
@@ -74,11 +78,11 @@ public class Usuario implements Serializable, IJson {
 		this.tipo = tipo;
 	}
 
-	public List<Categoria> getPreferencias() {
+	public Collection<Categoria> getPreferencias() {
 		return preferencias;
 	}
 
-	public void setPreferencias(List<Categoria> preferencias) {
+	public void setPreferencias(Collection<Categoria> preferencias) {
 		this.preferencias = preferencias;
 	}
 
@@ -95,15 +99,18 @@ public class Usuario implements Serializable, IJson {
 			try {
 				StringBuilder sbp = new StringBuilder();
 				sbp.append(",\"p\":[");
-				for(int i=0;i<preferencias.size();i++) {
-					if(i>0)
+				Iterator<Categoria> it = preferencias.iterator();
+				boolean nofirst = false;
+				while(it.hasNext()) {
+					if(nofirst)
 						sbp.append(",");
-					sbp.append(preferencias.get(i).toJson());
+					else
+						nofirst = true;
+					sbp.append(it.next().toJson());
 				}
 				sbp.append("]");
 				sb.append(sbp.toString());
 			}catch(Exception exc) {
-				
 			}
 		}
 		sb.append("}");
